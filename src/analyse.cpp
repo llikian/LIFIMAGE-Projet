d3556c544d93c0f3fe0c20a65b8a868fd82d16c4
Minas <mimosxou@gmail.com>
2025-03-10 15:34:43 +0100

    calcule la couleur moyenne a droite de l'image

--- src/analyse.cpp
+++ src/analyse.cpp
@@ -8,8 +8,24 @@
 
 int main() {
     Image puzzle = read_image("data/puzzle.jpg");
+    int width = puzzle.width() * 0.8;
 
+    Color c(0);
+    int n = 0;
+    for (int i = width ; i < puzzle.width(); ++i) {
+        for (int j = 0; j < puzzle.height(); ++j) {
+            c = c + srgb(puzzle(i, j));
+            ++n;
+        }
+    }
 
+    c = c/n;
+
+    for (int i = 0 ; i < puzzle.width(); ++i) {
+        for (int j = 0; j < puzzle.height(); ++j) {
+            puzzle(i, j) = c;
+        }
+    }
 
     write_image(puzzle, "data/processed_puzzle.jpg");
 
